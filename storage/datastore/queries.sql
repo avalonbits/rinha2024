8429b5d6e26e7860365c4fcda39fd944ff908015
Igor Cananea <icc@avalonbits.com>
2024-02-08 21:01:54 -0500

    Added missing  query file.

---
+++ storage/datastore/queries.sql
@@ -0,0 +1,9 @@
+-- name: CreateTransaction :exec
+INSERT INTO Transaction (cid, tid, value, description, created_at)
+       VALUES (?, ?, ?, ?, ?);
+
+-- name: GetBalance :one
+SELECT  FLOOR(SUM(value)) AS balance FROM Transaction WHERE cid = ?;
+
+-- name: TransactionHistory :many
+SELECT * FROM Transaction  WHERE cid = ? ORDER BY created_at DESC;
